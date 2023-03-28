732d8614d26e77f6199a2db2bc4d9af05dd3bf16
Nazariy90 <pinhvin123@gmail.com>
2023-03-28 20:32:10 +0300

    add hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const photosMark = createGalleryPhotos(galleryItems);
+
+function createGalleryPhotos(photos) {
+  return photos
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image lazyload"
+      src="${preview}" 
+      data-source="${original}" 
+      alt="${description}"/></a></div>`;
+    })
+    .join("");
+}
+galleryContainer.insertAdjacentHTML("beforeend", photosMark);
+
+const openModal = (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const imgSelected = e.target.getAttribute("data-source");
+
+  const instance = basicLightbox.create(`
+    <img src="${imgSelected}" width="800" height="600">
+`);
+  instance.show();
+};
+
+galleryContainer.addEventListener("click", openModal);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const photosMark = createGalleryPhotos(galleryItems);
 
-console.log(galleryItems);
+function createGalleryPhotos(photos) {
+  return photos
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image lazyload"
+      src="${preview}" 
+      data-source="${original}" 
+      alt="${description}"/></a></div>`;
+    })
+    .join("");
+}
+galleryContainer.insertAdjacentHTML("beforeend", photosMark);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  animationSpeed: 250,
+});
